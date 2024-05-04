d089270aa13f7c176e0daa024dab70b727b3fa3f
SebasPCDev <sebpa.16@gmail.com>
2024-05-04 15:14:05 -0500

    otra prueba

--- back/coco-backend-project/src/main.ts
+++ back/coco-backend-project/src/main.ts
@@ -6,5 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(loggerGlobal);
   await app.listen(3000);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();

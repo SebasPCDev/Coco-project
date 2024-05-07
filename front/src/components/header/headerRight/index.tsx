d3d8bad06b2efc2f7f29ccbb7365ae96375ef4be
daramirezsu06 <daramirezsu06@gmail.com>
2024-05-06 21:47:36 -0500

    fron basico con paleta de colores

--- front/src/components/header/headerRight/index.tsx
+++ front/src/components/header/headerRight/index.tsx
@@ -1,16 +1,24 @@
 "use client";
 import { RiMenuLine } from "@remixicon/react";
 import { useHeaderContext } from "@/components/headerconstext";
+import Link from "next/link";
 
 const HeaderRight = () => {
   const { setMenuActive, isMenuActive } = useHeaderContext();
   const handleClick = () => {
     setMenuActive(!isMenuActive);
-  }
+  };
   return (
-    <div onClick={handleClick}>
-      <RiMenuLine color="black" />
-    </div>
+    <>
+      <div onClick={handleClick} className="md:hidden">
+        <RiMenuLine color="white" />
+      </div>
+      <div className="hidden md:block">
+        <Link href="/login" className="text-custom-white" onClick={handleClick}>
+          <li>LOGIN</li>
+        </Link>
+      </div>
+    </>
   );
 };
 
--- front/src/components/header/index.tsx
+++ front/src/components/header/index.tsx
@@ -6,9 +6,11 @@ import HeaderRight from "./headerRight";
 const Header = () => {
   return (
     <HeaderProvider>
-      <header className="bg-slate-100 flex flex-row justify-between items-center p-5 ">
-        <HeaderLeft />
-        <HeaderRight />
+      <header className=" bg-custom-secondary py-4">
+        <div className="w-3/4 m-auto flex flex-row justify-between items-center ">
+          <HeaderLeft />
+          <HeaderRight />
+        </div>
       </header>
       <MenuResponsive />
     </HeaderProvider>

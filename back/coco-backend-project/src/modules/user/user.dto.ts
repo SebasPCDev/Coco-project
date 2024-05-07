6ab6db034f31021725e4435a60e3dacf95e44654
Mauricio Jourdan <jourdanmauricio@gmail.com>
2024-05-07 18:06:36 -0300

    feat(api/auth route): add JWT and endpoint GET /signin

--- back/coco-backend-project/src/modules/user/user.dto.ts
+++ back/coco-backend-project/src/modules/user/user.dto.ts
@@ -1,1 +1,11 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {}
+
+export class LoginUserDto {
+  @IsNotEmpty()
+  email: string;
+
+  @IsNotEmpty()
+  password: string;
+}

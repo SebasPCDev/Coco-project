367aa1548565e89fa7d0160e27a2457227bfe2fc
Mauricio Jourdan <jourdanmauricio@gmail.com>
2024-05-08 20:43:26 -0300

    feat(api/coworkings route): add proteccion endpoints

--- back/coco-backend-project/src/modules/coworkings/coworkings.controller.ts
+++ back/coco-backend-project/src/modules/coworkings/coworkings.controller.ts
@@ -5,38 +5,52 @@ import {
   Body,
   //Put,
   Param,
-  Delete,
+  //Delete,
+  UseGuards,
+  ParseUUIDPipe,
 } from '@nestjs/common';
-import { CoworkingsService } from './coworkings.service';
-import { ApiTags } from '@nestjs/swagger';
-import { CreateCoworkingsDto } from './coworkings.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UUID } from 'crypto';
 
+import { CoworkingsService } from './coworkings.service';
+import { CreateCoworkingsDto } from './coworkings.dto';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { RolesGuard } from 'src/guards/roles.guard';
+import { Public } from 'src/decorators/public.decorator';
+import { Roles } from 'src/decorators/roles.decorator';
+import { Role } from 'src/models/roles.enum';
+
 @ApiTags('corokings')
+@ApiBearerAuth()
+@UseGuards(AuthGuard)
 @Controller('coworkings')
 export class CoworkingsController {
-  constructor(private readonly coworkingService: CoworkingsService) {}
+  constructor(private readonly coworkingsService: CoworkingsService) {}
+
+  @Public()
+  @Get()
+  getAllCoworkings() {
+    return this.coworkingsService.getAllCoworkings();
+  }
+
+  @Public()
+  @Get(':id')
+  getCoworkingById(@Param('id', ParseUUIDPipe) id: UUID) {
+    return this.coworkingsService.getCoworkingById(id);
+  }
 
   @Post()
   create(@Body() data: CreateCoworkingsDto) {
     console.log(data);
-    return this.coworkingService.create(data);
+    return this.coworkingsService.create(data);
   }
 
+  @Roles(Role.SUPERADMIN)
+  @UseGuards(RolesGuard)
   @Post('activate')
   activateCoworking(@Body() data: { id: UUID }) {
     console.log(data);
-    return this.coworkingService.activateCoworking(data.id);
-  }
-
-  @Get()
-  getAllCoworkings() {
-    return this.coworkingService.getAllCoworkings();
-  }
-
-  @Get(':id')
-  getCoworkingById(@Param('id') id: string) {
-    return this.coworkingService.getCoworkingById(id);
+    return this.coworkingsService.activateCoworking(data.id);
   }
 
   /*   @Put(':id')
@@ -47,8 +61,8 @@ export class CoworkingsController {
     return this.coworkingService.update(+id, updateCoworkingDto);
   } */
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.coworkingService.remove(+id);
-  }
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.coworkingsService.remove(+id);
+  // }
 }

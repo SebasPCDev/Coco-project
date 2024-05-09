1ba132d45112e1665d88c3e4c37207b5a0986dfe
SebasPCDev <sebpa.16@gmail.com>
2024-05-09 16:26:44 -0500

    fix/timeopencloseoptions ajuste en los campos de hora open close para seleccionar cada 30 minutos

--- front/src/components/coworkingsForm/index.tsx
+++ front/src/components/coworkingsForm/index.tsx
@@ -12,10 +12,9 @@ import PostCoworkings from "../../../utils/posts/postCoworkings";
 
 // importamos la validacion de formularios
 import coworkingValidation from "../../../utils/formValidation/coworkingValidation";
-import ICoworkingsErrorInfo  from "../../../utils/types/coworkingFormErrorInterface";
+import ICoworkingsErrorInfo from "../../../utils/types/coworkingFormErrorInterface";
 
 import { useRouter } from "next/navigation";
-
 
 const CoworkingsForm = () => {
   const router = useRouter();
@@ -40,28 +39,40 @@ const CoworkingsForm = () => {
     type: "",
   });
 
-  const [coworkingInfoError, setCoworkingInfoError] = useState<ICoworkingsErrorInfo>({
-    name: "",
-    lastname: "",
-    phone: "",
-    email: "",
-    identification: "",
-    position: "",
-    companyName: "",
-    companyEmail: "",
-    companyPhone: "",
-    address: "",
-    website: "",
-    open: "",
-    close: "",
-    capacity: "",
-    status: "pending",
-    observation: "Esto es una observacion",
-    message: "",
-    type: "",
-  });
+  const [coworkingInfoError, setCoworkingInfoError] =
+    useState<ICoworkingsErrorInfo>({
+      name: "",
+      lastname: "",
+      phone: "",
+      email: "",
+      identification: "",
+      position: "",
+      companyName: "",
+      companyEmail: "",
+      companyPhone: "",
+      address: "",
+      website: "",
+      open: "",
+      close: "",
+      capacity: "",
+      status: "pending",
+      observation: "Esto es una observacion",
+      message: "",
+      type: "",
+    });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+
+    setCoworkingInfo({
+      ...coworkingInfo,
+      [name]: value,
+    });
+
+    console.log(coworkingInfo);
+  };
+
+  const handleChangeTime = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = e.target;
 
     setCoworkingInfo({
@@ -82,17 +93,31 @@ const CoworkingsForm = () => {
     }
   };
 
-  useEffect (() => {
+  useEffect(() => {
     const errors = coworkingValidation(coworkingInfo);
     setCoworkingInfoError(errors);
     console.log(errors);
   }, [coworkingInfo]);
 
+  //* Generador de horas cada 30 minutos con el horario establecido *****************
+  const generateTimeOptions = () => {
+    const options = [];
+    for (let hour = 6; hour < 24; hour++) {
+      for (let minute = 0; minute < 60; minute += 30) {
+        const formattedHour = hour.toString().padStart(2, "0");
+        const formattedMinute = minute.toString().padStart(2, "0");
+        options.push(`${formattedHour}:${formattedMinute}`);
+      }
+    }
+    return options;
+  };
+
   return (
     <>
       <form
         className="md:w-1/3 m-auto px-2 md:my-20 grid grid-cols-2 gap-4 "
-        onSubmit={handleSubmit}>
+        onSubmit={handleSubmit}
+      >
         <h1 className="text-3xl text-center">Soy Coworking</h1>
         {formDataCoworkings.map(
           ({ name, label, type, placeholder, required }) => {
@@ -101,15 +126,33 @@ const CoworkingsForm = () => {
                 <label className="flex gap-4 px-2" htmlFor={name}>
                   {label}
                 </label>
-                <input
-                  type={type}
-                  name={name}
-                  placeholder={placeholder}
-                  required={required}
-                  className="border-2 mx-1/3 py-2"
-                  onChange={handleChange}
-                  value={coworkingInfo[name as keyof ICoworkingsInfo]}
-                />
+                {name === "open" || name === "close" ? (
+                  <select
+                    name={name}
+                    required={required}
+                    className="border-2 mx-1/3 py-2"
+                    onChange={handleChangeTime}
+                    value={coworkingInfo[name as keyof ICoworkingsInfo]}
+                  >
+                    <option value="">Selecciona una hora</option>
+                    {generateTimeOptions().map((time) => (
+                      <option key={time} value={time}>
+                        {time}
+                      </option>
+                    ))}
+                  </select>
+                ) : (
+                  <input
+                    type={type}
+                    name={name}
+                    placeholder={placeholder}
+                    required={required}
+                    className="border-2 mx-1/3 py-2"
+                    onChange={handleChange}
+                    value={coworkingInfo[name as keyof ICoworkingsInfo]}
+                  />
+                )}
+
                 <p className="text-red-500">
                   {coworkingInfoError[name as keyof ICoworkingsInfo]}
                 </p>
@@ -119,7 +162,8 @@ const CoworkingsForm = () => {
         )}
         <button
           className="bg-slate-500 text-white p-2 rounded-lg my-4"
-          type="submit">
+          type="submit"
+        >
           Enviar Solicitud
         </button>
       </form>

a5da73e62134a5c857f28ba94e7b42926f298da9
SebasPCDev <sebpa.16@gmail.com>
2024-05-10 11:45:52 -0500

    indexcomponentescoworking fix conflicto de código

--- front/src/components/coworkings/index.tsx
+++ front/src/components/coworkings/index.tsx
@@ -1,12 +1,11 @@
 "use client";
-// import React, { useState, useEffect } from "react";
-// import GetCoworkings from "../../../utils/gets/getCoworkings";
-// import CoworkingCard from "./coworkingCard";
-// import IResponseCoworking from "../../../utils/types/coworkingsResponse";
+import React, { useState, useEffect } from "react";
+import GetCoworkings from "../../../utils/gets/getCoworkings";
+import CoworkingCard from "./coworkingCard";
+import IResponseCoworking from "../../../utils/types/coworkingsResponse";
 
-// const Coworkings: React.FC = (): React.ReactElement => {
-//   const [coworkings, setCoworkings] = useState<IResponseCoworking[]>([]);
-
+const Coworkings: React.FC = (): React.ReactElement => {
+  const [coworkings, setCoworkings] = useState<IResponseCoworking[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,23 +20,22 @@
       }
     };
 
+    fetchData();
+  }, []);
 
-//     fetchData();
-//   }, []);
-
-//   return (
-//     <>
-//       <h1 className="m-auto text-center my-6 text-3xl font-bold">
-//         Listado de Coworkings
-//       </h1>
-//       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:w-3/4 m-auto">
-//         {coworkings.map((coworking: IResponseCoworking) => (
-//           <div key={coworking.id}>
-//             <CoworkingCard coworking={coworking} />
-//           </div>
-//         ))}
-//       </div>
-//     </>
-//   );
-// };
-// export default Coworkings;
+  return (
+    <>
+      <h1 className="m-auto text-center my-6 text-3xl font-bold">
+        Listado de Coworkings
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:w-3/4 m-auto">
+        {coworkings.map((coworking: IResponseCoworking) => (
+          <div key={coworking.id}>
+            <CoworkingCard coworking={coworking} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+export default Coworkings;

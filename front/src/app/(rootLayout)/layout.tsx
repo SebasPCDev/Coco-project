692ea9f2b7e5b400add201701c6d011b2bdc5ea1
daramirezsu06 <daramirezsu06@gmail.com>
2024-05-10 22:45:55 -0500

    grupos de diñeo (layout)

--- front/src/app/(rootLayout)/layout.tsx
+++ front/src/app/(rootLayout)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
-import Header from "../components/header";
+import "../globals.css";
+import Header from "../../components/header";
 import { UserProvider } from "@/components/context";
 
 const inter = Inter({ subsets: ["latin"] });

667b88ce5cd1ad713b958d30cbff3d27826ae128
nicolaserd <530nicolas@gmail.com>
2024-05-12 03:22:25 -0500

    add : CDRUD falta el dto (amenities)

--- back/src/modules/amenities/amenities.controller.ts
+++ back/src/modules/amenities/amenities.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Amenities } from 'src/entities/amenities.entity';
+import { AmenitiesService } from './amenities.service';
+import { UUID } from 'crypto';
 
 @Controller('amenities')
-export class AmenitiesController {}
+export class AmenitiesController {
+    constructor(private readonly amenitiesService: AmenitiesService) {}
+
+  @Get()
+  findAll(): Promise<Amenities[]> {
+    return this.amenitiesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: UUID): Promise<Amenities> {
+    return this.amenitiesService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() amenityData: Partial<Amenities>): Promise<Amenities> {
+    return this.amenitiesService.create(amenityData);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: UUID, @Body() amenityData: Partial<Amenities>): Promise<Amenities> {
+    return this.amenitiesService.update(id, amenityData);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string): Promise<void> {
+    return this.amenitiesService.delete(+id);
+  }
+}
